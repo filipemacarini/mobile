b73d957facafc44aeef0d32c78e4b5b6081ce815
FilipeMacarini <filiperoco.github@yahoo.com>
2026-07-07 18:08:45 -0300

    feat: remove duplicatas

--- ex04.js
+++
@@ -1,7 +0,0 @@
-const input = require("readline-sync")
-
-let numero = parseFloat(input.question("Número: "))
-
-for (let i = 0; i <= 10; i++) {
-    console.log(`${numero} x ${i} = ${numero * i}`)
-}
